159e2276eeebd046ef2a9f021ac1c2a3d83dff9e
George Ionas <gionas@zipcoders-MacBook-Pro.local>
2020-07-24 10:26:37 -0400

    implemented Clock class

---
+++ src/main/java/org/example/Clock.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Clock {
+
+    private int time;
+
+    public Clock(int time){
+        this.time = time;
+    }
+
+    public void getTime(){
+        System.out.println(time);
+    }
+}
