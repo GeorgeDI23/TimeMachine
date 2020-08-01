d855397a5a62408c593d7504a537fea7e9b62dd1
Shane Reilly <sreilly@zipcoders-MacBook-Pro.local>
2020-08-01 15:31:05 -0400

    Practicing TDD, sortStringOfNums

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -10,4 +12,75 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    public void sortStringNumbers(String[] testArray) {
+        Boolean isSorted = false;
+        Arrays.sort(testArray);
+        System.out.println(Arrays.toString(testArray));
+        while(!isSorted){
+            isSorted = true;
+            for(int i = 0; i < testArray.length-1; i++) {
+                Boolean dontSwap = customComparator(testArray[i], testArray[i+1]);
+                if(!dontSwap){
+                    String temp = testArray[i];
+                    testArray[i] = testArray[i+1];
+                    testArray[i+1] = temp;
+                    isSorted = false;
+                }
+            }
+            System.out.println(Arrays.toString(testArray));
+        }
+        System.out.println(Arrays.toString(testArray));
+    }
+
+    public Boolean customComparator(String first, String second) {
+        if(first.charAt(0) == '-' && second.charAt(0) != '-'){
+            return true;
+        }else if(second.charAt(0) == '-' && first.charAt(0) != '-'){
+            return false;
+        }else if(first.charAt(0) == '-' && second.charAt(0) == '-'){
+            first = first.substring(1);
+            second = second.substring(1);
+            return !customComparator(first, second);
+        }
+
+        first = removeLeadingZeros(first);
+        second = removeLeadingZeros(second);
+
+        String firstNoDecimal = removeDecimal(first);
+        String secondNoDecimal = removeDecimal(second);
+
+        if(!firstNoDecimal.equals(secondNoDecimal)){
+            if(firstNoDecimal.length() > secondNoDecimal.length()){
+                return false;
+            }else if(secondNoDecimal.length() > firstNoDecimal.length()){
+                return true;
+            } else if(firstNoDecimal.compareTo(secondNoDecimal) > 0){
+                return false;
+            }
+        }else{
+            return !(first.compareTo(second) > 0);
+        }
+        return true;
+    }
+
+    public String removeDecimal(String input) {
+        for(int i = 0; i < input.length(); i++){
+            if(input.charAt(i) == '.'){
+                return input.substring(0, i);
+            }
+        }
+        return input;
+    }
+
+    public String removeLeadingZeros(String input){
+        for(int i = 0; i < input.length(); i++){
+            if(input.charAt(i) != '0'){
+                return input.substring(i);
+            }
+        }
+        return "0";
+    }
+
+
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,8 +1,12 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 /**
  * Unit test for simple App.
@@ -12,9 +16,264 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+
+    @BeforeEach
+    public void setUp(){
+        testArray1 = new String[]{"1", "5", "9", "4"};
+        testArray2 = new String[]{"1", "5", "15", "9", "4"};
+
+    }
+
+    App sort = new App();
+    String[] testArray1;
+    String[] testArray2;
+
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void sort_single_digit_numbers(){
+        // Given
+        String[] expected = new String[]{"1", "4", "5", "9"};
+        // When
+        sort.sortStringNumbers(testArray1);
+        // Then
+        assertArrayEquals(expected, testArray1);
+    }
+
+    @Test
+    // no edge cases
+    public void sort_a_normal_list(){
+        // Given
+        String[] expected = new String[]{"1", "4", "5", "9", "15"};
+        // When
+        sort.sortStringNumbers(testArray2);
+        // Then
+        assertArrayEquals(expected,testArray2);
+    }
+
+    @Test
+    // no edge cases
+    public void sort_a_list_with_larger_nums(){
+        // Given
+        String[] testArray = {"1", "9", "35", "5", "25", "4", "15"};
+        String[] expected = new String[]{"1", "4", "5", "9", "15", "25", "35"};
+        // When
+        sort.sortStringNumbers(testArray);
+        // Then
+        assertArrayEquals(expected,testArray);
+    }
+
+    @Test
+    // no edge cases
+    public void sort_a_list_with_leading_zeros(){
+        // Given
+        String[] testArray = {"1", "09", "35", "5", "025", "4", "15"};
+        String[] expected = new String[]{"1", "4", "5", "09", "15", "025", "35"};
+        // When
+        sort.sortStringNumbers(testArray);
+        // Then
+        assertArrayEquals(expected,testArray);
+    }
+
+    @Test
+    public void sort_a_list_with_more_zeros(){
+        // Given
+        String[] testArray = {"1", "09", "000", "35", "5", "025", "4", "15"};
+        String[] expected = new String[]{"000", "1", "4", "5", "09", "15", "025", "35"};
+        // When
+        sort.sortStringNumbers(testArray);
+        // Then
+        assertArrayEquals(expected,testArray);
+    }
+
+    @Test
+    public void sort_a_list_with_negatives(){
+        // Given
+        String[] testArray = {"1", "-9", "35", "5", "-25", "4", "15"};
+        String[] expected = new String[]{ "-25", "-9", "1", "4", "5", "15", "35"};
+        // When
+        sort.sortStringNumbers(testArray);
+        // Then
+        assertArrayEquals(expected,testArray);
+    }
+
+    @Test
+    public void sort_a_list_with_negatives_and_leading_zeros(){
+        // Given
+        String[] testArray = {"1", "-009", "35", "5", "-25", "0004", "015"};
+        String[] expected = new String[]{ "-25", "-009", "1", "0004", "5", "015", "35"};
+        // When
+        sort.sortStringNumbers(testArray);
+        // Then
+        assertArrayEquals(expected,testArray);
+    }
+
+    @Test
+    public void sort_a_list_with_decimals(){
+        // Given
+        String[] testArray = {"1", "9", "35", "5.24" ,"5.1444", "25", "4", "15"};
+        String[] expected = new String[]{"1", "4", "5.1444", "5.24", "9", "15", "25", "35"};
+        // When
+        sort.sortStringNumbers(testArray);
+        // Then
+        assertArrayEquals(expected,testArray);
+    }
+
+    @Test
+    public void sort_a_list_with_ALL_the_things(){
+        // Given
+        String[] testArray = {"005", "-7", "9", "-0008", "3.14", "-8.78", "002.5", "-0004.20"};
+        String[] expected = new String[]{"-8.78", "-0008", "-7", "-0004.20", "002.5", "3.14", "005", "9"};
+        // When
+        sort.sortStringNumbers(testArray);
+        // Then
+        assertArrayEquals(expected,testArray);
+    }
+
+    //submethod tests
+    @Test
+    public void testCustomComparator(){
+        //given
+        String numOne = "15";
+        String numTwo = "9";
+        //when
+        Boolean actual = sort.customComparator(numOne, numTwo);
+        //then
+        assertFalse(actual);
+    }
+
+    @Test
+    public void testCustomComparatorSameDigits(){
+        //given
+        String numOne = "15";
+        String numTwo = "25";
+        //when
+        Boolean actual = sort.customComparator(numOne, numTwo);
+        //then
+        assertTrue(actual);
+    }
+
+    @Test
+    public void testCustomComparatorLeadingZeros(){
+        //given
+        String numOne = "015";
+        String numTwo = "25";
+        //when
+        Boolean actual = sort.customComparator(numOne, numTwo);
+        //then
+        assertTrue(actual);
+    }
+
+    @Test
+    public void testCustomComparatorLeadingZeros2(){
+        //given
+        String numOne = "25";
+        String numTwo = "015";
+        //when
+        Boolean actual = sort.customComparator(numOne, numTwo);
+        //then
+        assertFalse(actual);
+    }
+    @Test
+    public void testCustomComparatorLeadingZeros3(){
+        //given
+        String numOne = "09";
+        String numTwo = "1";
+        //when
+        Boolean actual = sort.customComparator(numOne, numTwo);
+        //then
+        assertFalse(actual);
+    }
+
+    @Test
+    public void testCustomComparatorNegatives(){
+        //given
+        String numOne = "-2";
+        String numTwo = "1";
+        //when
+        Boolean actual = sort.customComparator(numOne, numTwo);
+        //then
+        assertTrue(actual);
+    }
+
+    @Test
+    public void testCustomComparatorNegatives2(){
+        //given
+        String numOne = "1";
+        String numTwo = "-2";
+        //when
+        Boolean actual = sort.customComparator(numOne, numTwo);
+        //then
+        assertFalse(actual);
+    }
+
+    @Test
+    public void testCustomComparatorTwoNegatives(){
+        //given
+        String numOne = "-1";
+        String numTwo = "-2";
+        //when
+        Boolean actual = sort.customComparator(numOne, numTwo);
+        //then
+        assertFalse(actual);
+    }
+
+    @Test
+    public void testCustomComparatorTwoNegatives2(){
+        //given
+        String numOne = "-2";
+        String numTwo = "-1";
+        //when
+        Boolean actual = sort.customComparator(numOne, numTwo);
+        //then
+        assertTrue(actual);
+    }
+
+    @Test
+    public void testRemoveLeadingZeros(){
+        //given
+        String num = "00000015";
+        String expected = "15";
+        //when
+        String actual = sort.removeLeadingZeros(num);
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemoveLeadingZerosWithAllZeros(){
+        //given
+        String num = "00000";
+        String expected = "0";
+        //when
+        String actual = sort.removeLeadingZeros(num);
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemoveDecimal(){
+        //given
+        String decimalNum = "3.14";
+        String expected = "3";
+        //when
+        String actual = sort.removeDecimal(decimalNum);
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemoveDecimalNoDecimal(){
+        //given
+        String decimalNum = "3";
+        String expected = "3";
+        //when
+        String actual = sort.removeDecimal(decimalNum);
+        //then
+        Assert.assertEquals(expected, actual);
     }
 }
